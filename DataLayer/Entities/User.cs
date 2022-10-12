8c19581893f3b5f2e6c4737c2fc155239e87eaa1
Explosy <devotion1996@gmail.com>
2022-10-12 16:01:08 +0300

    Refactoring

---
+++ DataLayer/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace DataLayer.Entities {
+  public partial class User {
+    public int Id { get; set; }
+    [Display(Name = "Имя")]
+    public string Name { get; set; }
+    [Display(Name = "Фамилия")]
+    public string Surname { get; set; }
+    [Display(Name = "Возраст")]
+    public int? Age { get; set; }
+    [Display(Name = "E-mail")]
+    public string Email { get; set; }
+    [Display(Name = "Дата регистрации")]
+    public DateTime? RegistationDate { get; set; }
+  }
+}
