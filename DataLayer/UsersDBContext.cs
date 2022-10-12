25d0f80e643866973e6602fc511fe98e3f896e2b
Explosy <devotion1996@gmail.com>
2022-10-12 15:51:38 +0300

    Refactoring

---
+++ DataLayer/UsersDBContext.cs
@@ -0,0 +1,44 @@
+﻿using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace DataLayer {
+  public partial class UsersDBContext : DbContext {
+    public UsersDBContext(DbContextOptions<UsersDBContext> options)
+        : base(options) {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+      modelBuilder.HasAnnotation("Relational:Collation", "Cyrillic_General_CI_AS");
+
+      modelBuilder.Entity<User>(entity => {
+        entity.ToTable("users");
+
+        entity.HasIndex(e => e.Email, "UQ__users__A9D105342BE8BF8F")
+            .IsUnique();
+
+        entity.Property(e => e.Age).HasDefaultValueSql("((0))");
+
+        entity.Property(e => e.Email)
+            .HasMaxLength(30)
+            .IsUnicode(false);
+
+        entity.Property(e => e.Name).HasMaxLength(30);
+
+        entity.Property(e => e.RegistationDate)
+            .HasColumnType("date")
+            .HasDefaultValueSql("(getdate())");
+
+        entity.Property(e => e.Surname).HasMaxLength(30);
+      });
+
+      OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+  }
+}
