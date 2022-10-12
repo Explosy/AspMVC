bf23285aae7ea24e16a356a695b8d1d73aecaada
Explosy <devotion1996@gmail.com>
2022-10-12 12:51:29 +0300

    Добавьте файлы проекта.

---
+++ DataLayer/Entityes/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace DataLayer.Entityes
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        [Display(Name = "Имя")]
+        public string Name { get; set; }
+        [Display(Name = "Фамилия")]
+        public string Surname { get; set; }
+        [Display(Name = "Возраст")]
+        public int? Age { get; set; }
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+        [Display(Name = "Дата регистрации")]
+        public DateTime? RegistationDate { get; set; }
+    }
+}
---
+++ DataLayer/Entityes/UsersDBContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace DataLayer.Entityes
+{
+    public partial class UsersDBContext : DbContext
+    {
+        public UsersDBContext(DbContextOptions<UsersDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Cyrillic_General_CI_AS");
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("users");
+
+                entity.HasIndex(e => e.Email, "UQ__users__A9D105342BE8BF8F")
+                    .IsUnique();
+
+                entity.Property(e => e.Age).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name).HasMaxLength(30);
+
+                entity.Property(e => e.RegistationDate)
+                    .HasColumnType("date")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Surname).HasMaxLength(30);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
