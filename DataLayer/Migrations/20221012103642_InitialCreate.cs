6b7568f1d60cc00bf63147c47135aa8c2dcf7994
Explosy <devotion1996@gmail.com>
2022-10-12 13:41:21 +0300

    Add migration

---
+++ DataLayer/Migrations/20221012103642_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataLayer.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: true),
+                    Surname = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: true, defaultValueSql: "((0))"),
+                    Email = table.Column<string>(type: "varchar(30)", unicode: false, maxLength: 30, nullable: true),
+                    RegistationDate = table.Column<DateTime>(type: "date", nullable: true, defaultValueSql: "(getdate())")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "UQ__users__A9D105342BE8BF8F",
+                table: "users",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
