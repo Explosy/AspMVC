7945c4639a635bbd8464e79e77365943fc7292b0
Explosy <devotion1996@gmail.com>
2022-10-15 19:17:22 +0300

    Add Check and Error message to Model

--- DTO/UserDTO.cs
+++ DTO/UserDTO.cs
@@ -8,12 +8,17 @@ namespace DTO {
   public class UserDTO {
     public int Id { get; set; }
     [Display(Name = "Имя")]
+    [Required (ErrorMessage ="Пожалуйста укажите имя")]
     public string Name { get; set; }
     [Display(Name = "Фамилия")]
+    [Required(ErrorMessage = "Пожалуйста укажите Фамилию")]
     public string Surname { get; set; }
     [Display(Name = "Возраст")]
+    [Required(ErrorMessage = "Пожалуйста укажите возраст!")]
     public int? Age { get; set; }
     [Display(Name = "E-mail")]
+    [Required (ErrorMessage = "Пожалуйста укажите e-mail")]
+    [RegularExpression (".+\\@.+\\..+",ErrorMessage = "Пожалуйста укажите e-mail")]
     public string Email { get; set; }
     [Display(Name = "Дата регистрации")]
     public DateTime? RegistationDate { get; set; }
